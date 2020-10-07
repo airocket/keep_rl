719bd2c1c23d0e6568f2ad3cafdcfd45669668e3
airocket <40832068+airocket@users.noreply.github.com>
2020-10-07 13:55:21 +0300

    Update main_keep_rl.py

--- main_keep_rl.py
+++ main_keep_rl.py
@@ -15,8 +15,9 @@ import time
 import tensorflow as tf
 import numpy as np
 import pandas.io.sql as psql
+import pprint
 
-from KeepTradingEnv import KeepTradingEnv
+from KeepTradingEnv_new2 import KeepTradingEnv
 
 
 def get_keep_data():
@@ -28,11 +29,13 @@ def get_keep_data():
 
 df = get_keep_data()
 df = df.sort_values('index')
+df.to_csv('keep_info.csv',index=False)
 df.drop(['index'], axis=1, inplace=True)
 df = df.astype(np.float64)
 dfTest = df
 
 if __name__ == '__main__':
+
     def make_envTest(rank, seed=0):
         def _init():
             env = KeepTradingEnv(dfTest)
@@ -67,35 +70,45 @@ if __name__ == '__main__':
 
     study = optuna.create_study(study_name='cartpol_optuna', storage='sqlite:///params.db', load_if_exists=True)
     model_params = get_model_params()
-    print(model_params)
+    #model_params['learning_rate'] = model_params['learning_rate']/200
+    model_params['gamma'] = 0.99
 
-    n_cpu = 4
+    print('Imput DataFrame:')
+    print(df.head())
+    print(df.tail())
+    pprint.pprint(model_params)
+
+    n_cpu = 6
+    learn_timesteps = 20000
+    test_timesteps = 3000
 
     env = SubprocVecEnv([make_env(i) for i in range(n_cpu)])  # задаем колл-во процессоров
     test_env = DummyVecEnv([make_envTest(i) for i in range(1)])
 
-    model = PPO2(MlpLnLstmPolicy, env, nminibatches=1, verbose=1, n_steps=49, tensorboard_log="./tensorboard_keep/",
-                 **model_params)
+    policy_kwargs1 = dict(n_lstm=512)
+    model = PPO2(MlpLnLstmPolicy, env, nminibatches=1, verbose=1, n_steps=49, tensorboard_log="./tensorboard_keep/",policy_kwargs=policy_kwargs1, **model_params)
+
+    #model = PPO2(MlpLnLstmPolicy, env, nminibatches=1, verbose=1, n_steps=50, tensorboard_log="./tensorboard_keep/")
     # model = PPO2.load("model/Darwin/Model_Mac.pkl", nminibatches=n_cpu, env=env, verbose=1, n_steps=18, tensorboard_log="./tensorboard/")
     model.is_tb_set = True
 
     for n_epoch in range(0, 10):
         summary_writer = tf.compat.v1.summary.FileWriter("./tensorboard_keep/" + "Keep_trade_test_" + str(n_epoch+1))
-        print('\x1b[6;30;42m' + '***************************  Calculate epoch:', n_epoch,
-              '***************************' + '\x1b[0m')
+        print('\x1b[6;30;42m' + '**************  Calculate epoch:', n_epoch, '**************' + '\x1b[0m')
         time_learn = time.time()
-        save_s = model.learn(total_timesteps=20000, tb_log_name='Keep_learn')
+        save_s = model.learn(total_timesteps=learn_timesteps, tb_log_name='Keep_learn')
         delta_time_learn = time.time() - time_learn
-        print('Learn end', ' epoch:', n_epoch, ' time to work:', int((delta_time_learn) / 60), ' m')
-        print('FPS: ', 1000000 / delta_time_learn)
+        print(datetime.now(), 'Learn end', ' epoch:', n_epoch,' steps:', learn_timesteps, ' time to work:', int((delta_time_learn) / 60), 'min', ' FPS: ', round(learn_timesteps / delta_time_learn,1))
+        print()
         zero_completed_obs = np.zeros((n_cpu,) + env.observation_space.shape)
         zero_completed_obs[0, :] = test_env.reset()
         state = None
         reward_sum = 0
+        last_done_net_worth = 0
         all_net_worth_np = np.array([])
         time_test = time.time()
-        for i in range(5000):
-            action, _states = model.predict(zero_completed_obs, state=state)
+        for i in range(test_timesteps):
+            action, states = model.predict(zero_completed_obs, state=state)
             obs, reward, done, info = test_env.step(action)
             zero_completed_obs[0, :] = obs
             reward_sum += reward[0]
@@ -110,17 +123,17 @@ if __name__ == '__main__':
                     value=[tf.Summary.Value(tag='net_worth/' + str(n_epoch + 1), simple_value=net_worth)])
                 summary_writer.add_summary(summary, i)
                 reward_sum = 0
+                zero_completed_obs[0, :] = test_env.reset()
+                state = None
 
         average_net_worth = int(np.sum([all_net_worth_np]) / len(all_net_worth_np))
         delta_time_test = time.time() - time_test
-        print('Test  end', ' epoch:', n_epoch, ' time to work:', int((delta_time_test) / 60), ' m')
-        print('FPS: ', 5000 / delta_time_test)
-        print("Last done net worth in test: ", last_done_net_worth)
-        print('Average net_worth in test:', average_net_worth)
+        print(datetime.now(), 'Test end', ' epoch:', n_epoch,' steps:', test_timesteps, ' time to work:', int((delta_time_test) / 60), 'min', ' FPS: ', round(learn_timesteps / delta_time_test,1))
+        print(datetime.now(), "Last net worth test: ", round(last_done_net_worth), 'Average net_worth test:', round(average_net_worth))
         summary_writer.close()
         model_path = path.join('model', platform.system(), f'model_epoch_{n_epoch}.pkl')
         model.save(model_path)
         time.sleep(10)
-        print('Model saved to :', model_path)
+        print(datetime.now(), 'Model saved to :', model_path)
 
     time.sleep(10)
